385d15b97d3744803de6a9c66e7e5f25e9270b78
Gurpreet Ghai <gghai@codeaurora.org>
2018-03-12 14:52:57 +0530

    raphael: BT: Enable use of QTI stack on the device
    It is library for Bluetooth stack to be opened
    by Bluetooth apps and contains BT protocol
    implementations with qcom specific add ons.
    
    This change enables compilation and use of
    QTI stack for Bluetooth.
    
    Change-Id: I32bcf8faa93dff8d6fd655983a7ec90164bc2a28
    CRs-Fixed: 2203894

--- msmnile.mk
+++ msmnile.mk
@@ -114,7 +114,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    liba2dpoffload \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
+    libbluetooth_qti \
     libbthost_if \
     libldacBT_dec \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
