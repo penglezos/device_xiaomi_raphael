eab39057fa2cc8951b2a99964383a24658de8f2e
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    raphael: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- device.mk
+++ device.mk
@@ -15,6 +15,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
