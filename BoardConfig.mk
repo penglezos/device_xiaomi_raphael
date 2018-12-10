9d925bf171ab91b1b363a431f513946d9009db96
Kilian von Pflugk <github@jumoog.io>
2018-12-10 12:31:27 +0100

    raphael: remove TARGET_USES_64_BIT_BINDER
    the TARGET_USES_64_BIT_BINDER option is deprecated and can be removed
    
    Change-Id: I6b828da1fbcc8c054c5b35ff78c870a689f7ed9f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,8 +36,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a75
 
-TARGET_USES_64_BIT_BINDER := true
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := raphael,raphaelin
 
