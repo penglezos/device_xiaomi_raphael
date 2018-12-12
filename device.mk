d8966630f32267bc13146d254bfea9ff279a54b9
Anton Hansson <hansson@google.com>
2018-12-12 16:15:57 +0000

    raphael: Remove PRODUCT_AAPT config
    This config strips unused drawable resources from the device
    to save space, but it is not particularly effective in practice.
    (roughly ~30 MB across all partitions).
    
    Remove it to make the system image more generic.
    
    Bug: 118676832
    Test: make
    Change-Id: I429dc449866fc0b147cade64c42cf41b00698ffe

--- device.mk
+++ device.mk
@@ -43,10 +43,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
