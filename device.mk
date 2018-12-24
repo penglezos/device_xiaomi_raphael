d7e57242d42cd19401b45352e2ce6281fb2a7633
dianlujitao <dianlujitao@lineageos.org>
2018-12-24 12:04:48 +0800

    raphael: Support Alipay fingerprint payment
     * IFAA manager is based on OnePlusOSS, but adapted for Xiaomi's mlipay
       interface.
    
    Change-Id: Ied17d6456561bb399e4dd4a868d57a48d2d51db2

--- device.mk
+++ device.mk
@@ -235,6 +235,13 @@ PRODUCT_PACKAGES += \
     init.xiaomi.rc \
     ueventd.qcom.rc
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
