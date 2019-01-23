04d043a10ad91eafe1f57e8e57afbe58747f3843
Bruno Martins <bgcngm@gmail.com>
2019-01-23 10:25:44 +0000

    raphael: Implement SunlightEnhancement LiveDisplay HAL
    Change-Id: I17b0215ad78d86989c146291552c5e6e7b6a689d

--- device.mk
+++ device.mk
@@ -258,7 +258,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service.xiaomi_raphael
 
 # Media
 PRODUCT_PACKAGES += \
