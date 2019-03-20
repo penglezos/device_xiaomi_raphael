e3a8d4900b8025fc3ca5c0870226fe7a7cf02fea
Jenhao Chen <jenhaochen@google.com>
2019-03-20 14:57:51 -0700

    raphael: Enable zram writeback
    Bug: 128889899
    Test: Boot to home
    
    Change-Id: I48ae06e54a66a7d6965afbe8db3d2f157cf8a1f4
    
    Former-commit-id: 86eed1a4a6a27a1adff335c0409f8966556e1e1c

--- device.mk
+++ device.mk
@@ -377,6 +377,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/msmnile/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# ZRAM writeback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
+
 # QTI
 PRODUCT_PACKAGES += \
     libjson \
