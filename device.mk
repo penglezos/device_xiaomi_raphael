8eb666613ce2cefa0bfa18e212964dfb7bd1e170
Demon000 <demonsingur@gmail.com>
2019-05-14 19:32:11 +0000

    raphael: Import lights HAL from Xiaomi msm8998-common
    Change-Id: Ieee4277f15927b4522deaec9a709809cf6db6fea

--- device.mk
+++ device.mk
@@ -236,6 +236,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_raphael
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
