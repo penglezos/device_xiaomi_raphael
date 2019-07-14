6d6dd4a54fdd243f6f603f3a6b643fd66dad00e5
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-07-14 18:57:17 +0200

    raphael: Build common Trust HAL
    Change-Id: I73a24535534a696ae42cdda3195c60aa2e50ff33

--- device.mk
+++ device.mk
@@ -359,6 +359,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
