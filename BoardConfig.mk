7bee65667fd4911e896448fbb271e66fad2c8de8
Lijuan Gao <lijuang@codeaurora.org>
2019-07-15 18:55:50 +0800

    raphael: Add support for header version 2 of boot image
    Adding support to make a bootimage with header version as 2.
    This bootimage no longer has the dtbs appended to the kernel.
    All the dtbs are made into a new image called dtb.img and
    this image is present in the bootimage.
    
    Change-Id: I47867fc284a45bcf2e54af5f6cd40d894418c002
    Signed-off-by: Lijuan Gao <lijuang@codeaurora.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,16 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += msm_drm.timing_override=1
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
+#Disable appended dtb
+TARGET_KERNEL_APPEND_DTB := true
+# Set Header version for bootimage
+ifneq ($(strip $(TARGET_KERNEL_APPEND_DTB)),true)
+#Enable dtb in boot image and Set Header version
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_BOOTIMG_HEADER_VERSION := 2
+else
 BOARD_BOOTIMG_HEADER_VERSION := 1
+endif
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
