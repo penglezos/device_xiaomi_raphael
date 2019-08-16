1eb08adbbb993cb0113bcc2b9c5bd4b93798fe2e
Wang Han <416810799@qq.com>
2019-08-16 21:22:23 +0800

    raphael: Clear persist.vendor.usb.config on boot
     * QCOM init scripts set every possible debug option if USB modes
       are not set on boot. This is expensive and causes USB debugging
       to be enabled on boot every time. OEMs often fix this by
       themselves, so do us.
    
     * Note, once this prop is set, the issue can never be easily fixed
       as it is a persist prop. As we want to satisfy users without
       wiping data or playing with command line, just do
       "setprop persist.vendor.usb.config """ unconditionally.
    
      https://gitlab.com/LineageOS/issues/android/issues/308
    
    Change-Id: I9c7175c085de6c0eb278ce8631b518d27e011065

--- rootdir/etc/init.mi.usb.sh
+++ rootdir/etc/init.mi.usb.sh
@@ -5,3 +5,6 @@ if [ "$(getprop persist.vendor.usb.config)" != "$usb_config" -a \
         "$(getprop ro.boot.factorybuild)" != "1" ];then
      setprop persist.vendor.usb.config "$usb_config"
 fi
+
+# Clear vendor USB config because it is only needed for debugging
+setprop persist.vendor.usb.config ""
