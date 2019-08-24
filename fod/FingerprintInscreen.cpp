703e2725647777846cf3c5428a1bf41de88a560c
Akash <akashniki@gmail.com>
2019-08-24 09:14:16 +0000

    cepheus:fod: Update fod position

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -34,9 +34,9 @@
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF 0
 
-#define FOD_SENSOR_X 455
-#define FOD_SENSOR_Y 1920
-#define FOD_SENSOR_SIZE 173
+#define FOD_SENSOR_X 453
+#define FOD_SENSOR_Y 1918
+#define FOD_SENSOR_SIZE 174
 
 namespace {
 
