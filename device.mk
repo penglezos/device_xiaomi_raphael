d212b5780b106ad6e0fed475d1b4b958026cec4a
Tharaga Balachandran <tbalacha@codeaurora.org>
2019-09-26 19:17:58 -0400

    raphael: gralloc: Upgrade mapper and allocators to v3.0
    CRs-Fixed: 2445393
    Change-Id: I8c44ab63ca81523bb6acb75e8ae878cc7466e139

--- device.mk
+++ device.mk
@@ -165,7 +165,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.mapper@2.0-impl-qti-display
+    android.hardware.graphics.mapper@3.0-impl-qti-display
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor
