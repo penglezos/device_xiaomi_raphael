7e61ed638cf467735d314e5d42ae1c66653d2099
Ethan Chen <intervigil@gmail.com>
2019-10-07 16:43:32 -0400

    raphael: Add AOSP RCS packages
    Change-Id: Ic16d3b92820d246db73ad70ccaa0f4bc7f50e515
    Signed-off-by: Akash <akashniki@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -353,6 +353,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product_privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \
