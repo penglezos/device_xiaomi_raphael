4b262a41d747722a60eadf88be29bbd542ad7f87
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2019-10-31 23:59:34 +0530

    cepheus: Use CAF vendor framework matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,7 +106,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Lights
