40e91f3fa7e9741aef27d08bd2f4064a27b7abd6
Demon000 <demonsingur@gmail.com>
2019-11-20 13:25:49 +0100

    sm8150-common: fod: adjust nit value used based on screen brightness
    Change-Id: I4307b49917333b90356067d5333fdb4cce4c4844

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -23,7 +23,8 @@
 #include <cmath>
 
 #define COMMAND_NIT 10
-#define PARAM_NIT_FOD 3
+#define PARAM_NIT_630_FOD 1
+#define PARAM_NIT_300_FOD 4
 #define PARAM_NIT_NONE 0
 
 #define FOD_HBM_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_hbm"
@@ -92,7 +93,13 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 
 Return<void> FingerprintInscreen::onPress() {
     set(FOD_HBM_PATH, FOD_HBM_ON);
-    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_FOD);
+
+    if (get(BRIGHTNESS_PATH, 0) > 100) {
+        xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_300_FOD);
+    } else if (get(BRIGHTNESS_PATH, 0) != 0) {
+        xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_630_FOD);
+    }
+
     return Void();
 }
 
