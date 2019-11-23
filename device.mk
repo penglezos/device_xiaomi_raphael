b7c0dfd43304840657c4bbfe87c7cc22aa704065
Mikhail Naganov <mnaganov@google.com>
2019-11-22 17:34:45 -0800

    raphael: Switch to Audio HAL V6
    Switch coral and flame to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                      manual audio "smoke" test on coral
    
    Change-Id: If40ae28c224160728634736ee95590a779f905c4

--- device.mk
+++ device.mk
@@ -52,8 +52,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.1-impl
 
