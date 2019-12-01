b3fa077ae782d08863019504ea88b73e5d6e50d6
Akash <akashniki@gmail.com>
2019-12-01 08:42:05 +0000

    cepheus: Add hotword enrollment support

--- msmnile.mk
+++ msmnile.mk
@@ -65,6 +65,11 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# Hotword Enrollment
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml
+
 # Init
 PRODUCT_PACKAGES += \
     init.mi_thermald.rc \
