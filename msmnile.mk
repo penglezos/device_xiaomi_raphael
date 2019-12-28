f1170db9c3ec52ea0b4acd53b900453f18d8f2e1
PIPIPIG233666 <pig.priv@gmail.com>
2019-12-27 20:33:55 -0500

    raphael: Build vendor.qti.hardware.btconfigstore@1.0 from source
    Change-Id: Id3fdd9dc8df011cb5652ab11b2ab9778dcf5c80a

--- msmnile.mk
+++ msmnile.mk
@@ -119,7 +119,8 @@ PRODUCT_PACKAGES += \
     libbluetooth_qti \
     libbthost_if \
     libldacBT_dec \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
