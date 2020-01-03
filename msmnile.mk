e0a395a36905e30b1232be42591a115b96dfe511
Demon Singur <demonsingur@gmail.com>
2020-01-04 03:25:38 +0530

    cepheus: Build vulkan stuff

--- msmnile.mk
+++ msmnile.mk
@@ -373,6 +373,15 @@ PRODUCT_COPY_FILES += \
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 29
 
+# Vulkan
+PRODUCT_PACKAGES += \
+    libvulkan
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     wifi-mac-generator
