3f0811dca14c3f2213d4f2780e170be3c0ff41f9
Akash <akashniki@gmail.com>
2020-01-04 03:26:30 +0530

    cepheus: Remove unneeded VNDK flag

--- msmnile.mk
+++ msmnile.mk
@@ -370,9 +370,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 29
-
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
