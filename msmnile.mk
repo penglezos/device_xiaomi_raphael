8274ed1067c919be2801f47af2b35c258b4becc2
Demon Singur <demonsingur@gmail.com>
2020-01-04 03:06:29 +0530

    cepheus: Copy needed camera permissions

--- msmnile.mk
+++ msmnile.mk
@@ -120,6 +120,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
