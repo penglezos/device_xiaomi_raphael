0a8797edc9c94f78a5d01bea2dccf95b8ace38b2
Akash <akashniki@gmail.com>
2020-01-04 01:44:44 +0530

    cepheus: Import libnfc-nxp.conf

--- msmnile.mk
+++ msmnile.mk
@@ -204,6 +204,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # NN
 PRODUCT_PACKAGES += \
