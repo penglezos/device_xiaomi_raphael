4f794b49b226413419be2f14098568bc51b5f68a
Akash <akashniki@gmail.com>
2020-01-03 17:36:10 +0530

    cepheus: Import compatibility_matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
