5696e38c9723c749e814c19ede47933577ed4f26
Akash <akashniki@gmail.com>
2020-01-04 02:32:48 +0530

    cepheus: Rename system_prop to device_props

--- msmnile.mk
+++ msmnile.mk
@@ -28,7 +28,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-pa
 
 # Properties
--include $(LOCAL_PATH)/system_prop.mk
+-include $(LOCAL_PATH)/device_props.mk
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
