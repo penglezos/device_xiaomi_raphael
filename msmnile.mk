ec8cb0f35c3dc19382d567a5210168c57ceb5d48
Akash <akashniki@gmail.com>
2020-01-04 03:05:53 +0530

    cepheus: Build libdng_sdk from source

--- msmnile.mk
+++ msmnile.mk
@@ -116,6 +116,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    libdng_sdk.vendor \
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
