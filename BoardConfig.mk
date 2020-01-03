f09cced1b470524a6d9cd256ff22e18573468afb
Akash <akashniki@gmail.com>
2020-01-03 17:31:31 +0530

    cepheus: Import manifest.xml from Miui

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
