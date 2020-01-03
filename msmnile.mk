150f786dba411a584a73829dff58b9b9dbe7ba36
Akash <akashniki@gmail.com>
2020-01-04 03:10:00 +0530

    cepheus: Copy fstab to vendor

--- msmnile.mk
+++ msmnile.mk
@@ -146,6 +146,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
+
 # GPS
 PRODUCT_PACKAGES += \
     libsensorndkbridge
