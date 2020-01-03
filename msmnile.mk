082c4235690cba858430f714ccf5278c2017cdb1
Demon Singur <demonsingur@gmail.com>
2020-01-04 03:24:21 +0530

    cepheus: Copy touchscreen permission file to vendor

--- msmnile.mk
+++ msmnile.mk
@@ -358,6 +358,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Touchscreen
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 29
 
