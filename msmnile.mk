e4e6cd1deaeb71d15cadf6ebbee480405202c644
Akash <akashniki@gmail.com>
2020-01-04 03:35:53 +0530

    cepheus: Import keylayout from stock

--- msmnile.mk
+++ msmnile.mk
@@ -215,6 +215,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_msmnile
