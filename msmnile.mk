2e438bad046669a2ada0d9bd8fb07f5eae574738
Akash <akashniki@gmail.com>
2020-01-10 00:12:47 +0100

    cepheus: Build codec2 libs from source

--- msmnile.mk
+++ msmnile.mk
@@ -128,6 +128,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Codec2
+PRODUCT_PACKAGES += \
+    libcodec2_vndk.vendor \
+    libcodec2_hidl@1.0.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.display.config@1.10.vendor
