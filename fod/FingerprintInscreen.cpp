040cec24c15bd625556e436ea3454e2e3b233fc1
Akash <akashniki@gmail.com>
2020-01-27 17:20:06 +0530

    raphael: Update fingerprint position for Raphael

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -35,9 +35,9 @@
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF 0
 
-#define FOD_SENSOR_X 455
-#define FOD_SENSOR_Y 1920
-#define FOD_SENSOR_SIZE 173
+#define FOD_SENSOR_X 445
+#define FOD_SENSOR_Y 1931
+#define FOD_SENSOR_SIZE 190
 
 #define BRIGHTNESS_PATH "/sys/class/backlight/panel0-backlight/brightness"
 
