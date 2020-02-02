06468b5e74373e21f7b74620c9268d1ffeb9fe87
PIPIPIG233666 <pig.priv@gmail.com>
2020-02-02 08:21:16 -0500

    raphael: Disable UBWC
    Change-Id: If28a6bce43640b854a66b51944575a892da6bdbc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,6 +80,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_raphael
 TARGET_RECOVERY_DEVICE_MODULES := libinit_raphael
 
 # Media
+TARGET_DISABLED_UBWC := true
 TARGET_USES_ION := true
 
 # Partitions
