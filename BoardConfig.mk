6ffb3a65d3a3fd77050ed0b1df511f9ee18d4315
Nicolas Geoffray <ngeoffray@google.com>
2020-02-07 14:02:18 +0000

    raphael: overlay: Update in pinned files.
    Remove or update paths that don't exist.
    
    Bug: 119800099
    Test: boots
    Change-Id: Ib2cea88499cf08274d620d9d85ffcafc31fee868

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,9 +20,6 @@ DEVICE_PATH := device/xiaomi/raphael
 
 BUILD_BROKEN_DUP_RULES := true
 
-# APEX
-DEXPREOPT_GENERATE_APEX_IMAGE := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
