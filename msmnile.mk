ce09faf199f2706c62cf039f936875483c9c00e9
Akash <akashniki@gmail.com>
2020-03-01 12:30:41 +0000

    raphael: Build thermal.msmnile from source

--- msmnile.mk
+++ msmnile.mk
@@ -390,7 +390,8 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.msmnile
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
