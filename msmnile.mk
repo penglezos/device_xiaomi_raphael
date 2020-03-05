aa49c0addafc23be983db6c4f57916615bffe9b8
Akash <akashniki@gmail.com>
2020-03-05 10:10:23 +0000

    Revert "raphael: Drop Hotword support"
    This reverts commit c27f7be42f5248b3f539c6b09fb3621ed470f2b7.

--- msmnile.mk
+++ msmnile.mk
@@ -210,6 +210,11 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
 
+# Hotword Enrollment
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # IFAA manager
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
