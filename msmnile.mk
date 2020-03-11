05431dc3719e48fb2f46986105f9e68714f7e191
Akash Srivastava <akashniki@gmail.com>
2020-03-11 20:27:10 +0100

    raphael: Build Pixel live wallpapers

--- msmnile.mk
+++ msmnile.mk
@@ -427,6 +427,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
+# Wallpapers
+PRODUCT_PACKAGES += \
+    PixelLiveWallpaperPrebuilt
+
 # WiFi
 PRODUCT_PACKAGES += \
     wifi-mac-generator
