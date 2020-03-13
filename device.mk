91e0ecceb7d1cf2502da2dd48408fd03235f66d1
Sujin Panicker <spanic@codeaurora.org>
2020-03-13 17:21:28 +0530

    raphael: audio: Add mixer controls for call screen support
    Add mixer controls for proxy ports.
    Enable call screen mode support in audio_policy_config.
    
    Change-Id: Iaf4482909f05132ce987e24ed8e6f7991a4e5e9c

--- device.mk
+++ device.mk
@@ -116,6 +116,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8150/audio/configs/msmnile/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt \
+    hardware/qcom-caf/sm8150/audio/configs/msmnile/mixer_paths_pahu.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pahu.xml \
     hardware/qcom-caf/sm8150/audio/configs/msmnile/graphite_ipc_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/graphite_ipc_platform_info.xml
 
 # Bluetooth
