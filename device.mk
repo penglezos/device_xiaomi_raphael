61607fe0f9beb20740ae4b663e99f56409cefcd8
Lucas Dupin <dupin@google.com>
2020-03-23 10:55:10 -0700

    raphael: Default blurs to on on coral and flame
    Bug: 149792636
    Test: make
    Change-Id: Id78b72bd3925db21c4c963298c3684e3b54ba43a

--- device.mk
+++ device.mk
@@ -191,10 +191,9 @@ PRODUCT_PACKAGES += \
 # Enable incremental FS feature
 PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
 
-# Enable blurs, hidden under dev option
+# Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1
 
 # Fingerprint
