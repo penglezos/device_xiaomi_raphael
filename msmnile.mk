f640124cf6a898d87313645d39f1f4a57a6d24c6
Demon000 <demonsingur@gmail.com>
2020-03-23 13:23:04 +0100

    raphael: Import codec2 seccomp policy
    Change-Id: Ic5b921c241a128ff05ca073f6bb035e584f6da91

--- msmnile.mk
+++ msmnile.mk
@@ -264,6 +264,7 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 PRODUCT_COPY_FILES += \
