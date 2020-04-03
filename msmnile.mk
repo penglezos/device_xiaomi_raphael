d10d61fd2113fa4221a0da42ce8264075a09fb2b
Nauval Rizky <enuma.alrizky@gmail.com>
2020-04-03 21:05:45 +0900

    raphael: Import stock additional native libs list
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -287,6 +287,10 @@ PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8150/media/conf_files/msmnile/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
     hardware/qcom-caf/sm8150/media/conf_files/msmnile/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
+# Native Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
