8e57040904dc8ea0b197507f39a6915b4e752de6
Ashu7397 <ashutoshtaklikar@gmail.com>
2020-04-30 13:06:47 +0300

    raphael: lights: Change LED behavior
    Raphael has 2 LED lights. One on left of the popup (Name is green) and on on the right of popup (Name is blue). Don't know why it is named so weirdly. According to stock behavior, only the left LED should pulse on notification or battery as indicator since left side has a small circular cutout to pass the light. To mimick that, I removed the the right LED defination, since it is never used for notifications or battery light in stock, and if is enabled, causes a light bleed from other side of popup.

--- light/Light.cpp
+++ light/Light.cpp
@@ -23,8 +23,8 @@
 #include <fstream>
 
 #define LCD_LED         "/sys/class/backlight/panel0-backlight/"
-#define BLUE_LED        "/sys/class/leds/blue/"
-#define GREEN_LED       "/sys/class/leds/green/"
+//#define BLUE_LED        "/sys/class/leds/blue/"  //Right LED
+#define GREEN_LED       "/sys/class/leds/green/"  //Left LED
 
 #define BREATH          "breath"
 #define BRIGHTNESS      "brightness"
@@ -92,13 +92,9 @@ static void handleBacklight(const LightState& state) {
 }
 
 static void handleNotification(const LightState& state) {
-    uint32_t blueBrightness = getScaledBrightness(state, MAX_LED_BRIGHTNESS);
     uint32_t greenBrightness = getScaledBrightness(state, MAX_LED_BRIGHTNESS);
 
     /* Disable breathing or blinking */
-    set(BLUE_LED BREATH, 0);
-    set(BLUE_LED DELAY_OFF, 0);
-    set(BLUE_LED DELAY_ON, 0);
 
     set(GREEN_LED BREATH, 0);
     set(GREEN_LED DELAY_OFF, 0);
@@ -107,19 +103,15 @@ static void handleNotification(const LightState& state) {
     switch (state.flashMode) {
         case Flash::HARDWARE:
             /* Breathing */  
-            set(BLUE_LED BREATH, 1);
             set(GREEN_LED BREATH, 1);
             break;
         case Flash::TIMED:
             /* Blinking */
-            set(BLUE_LED DELAY_OFF, state.flashOnMs);
-            set(BLUE_LED DELAY_ON, state.flashOffMs);
             set(GREEN_LED DELAY_OFF, state.flashOnMs);
             set(GREEN_LED DELAY_ON, state.flashOffMs);
             break;
         case Flash::NONE:
         default:
-            set(BLUE_LED BRIGHTNESS, blueBrightness);
             set(GREEN_LED BRIGHTNESS, greenBrightness);
     }
 }
