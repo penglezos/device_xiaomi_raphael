bb777fc77146bb96536ac7fe685bc0557d1e2fc7
Akash <akashniki@gmail.com>
2020-05-11 08:25:13 +0200

    raphael: Switch to may fingerprint

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -57,5 +57,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V11.0.8.0 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys");
 }
