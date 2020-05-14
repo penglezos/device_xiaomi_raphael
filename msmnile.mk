acd89c4065bb08d7525822ee7ef9691c8c2a83d9
Akash <akashniki@gmail.com>
2020-05-14 11:30:43 +0200

    raphael: Build wifidisplay blobs from source

--- msmnile.mk
+++ msmnile.mk
@@ -459,9 +459,11 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libnl \
+    libdisplayconfig \
     libdisplayconfig.vendor \
-    libqdMetaData
+    libnl \
+    libqdMetaData \
+    libqdMetaData.system
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
