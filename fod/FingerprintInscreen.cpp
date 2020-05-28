7668ce0380eff1edaaf2ace0ad0eb6495b6707ff
Demon000 <demonsingur@gmail.com>
2020-05-29 00:40:01 +0200

    raphael: fod: Drop \n from logs
    Change-Id: I1ec1e1bbdb1bf6d034bba05cd653458f04d5e544

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -113,12 +113,12 @@ Return<void> FingerprintInscreen::onHideFODView() {
 }
 
 Return<bool> FingerprintInscreen::handleAcquired(int32_t acquiredInfo, int32_t vendorCode) {
-    LOG(ERROR) << "acquiredInfo: " << acquiredInfo << ", vendorCode: " << vendorCode << "\n";
+    LOG(ERROR) << "acquiredInfo: " << acquiredInfo << ", vendorCode: " << vendorCode;
     return false;
 }
 
 Return<bool> FingerprintInscreen::handleError(int32_t error, int32_t vendorCode) {
-    LOG(ERROR) << "error: " << error << ", vendorCode: " << vendorCode << "\n";
+    LOG(ERROR) << "error: " << error << ", vendorCode: " << vendorCode;
     return false;
 }
 
