8c372725f6ba3f39148aa540ad6949e7c0b66a57
Jyotiraditya <jyotiraditya@aospa.co>
2020-06-24 13:07:55 +0200

    raphael: parts: Implement Clear Speaker
    Co-authored-by: Adithya R <radithya2002@gmail.com>

---
+++ parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2020 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.speaker;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ClearSpeakerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
+        ClearSpeakerFragment clearSpeakerFragment;
+        if (fragment == null) {
+            clearSpeakerFragment = new ClearSpeakerFragment();
+            getFragmentManager().beginTransaction()
+                    .add(android.R.id.content, clearSpeakerFragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
