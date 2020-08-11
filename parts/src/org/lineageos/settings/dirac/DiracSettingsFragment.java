2577be91ac9613d1225bac8d330d56197a39a25b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-08-11 10:12:44 -0300

    raphael: parts: Disable hifi preference when toggling switchbar

--- parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
+++ parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -72,6 +72,7 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         boolean hifiEnable = DiracUtils.getHifiMode();
         mHeadsetType.setEnabled(!hifiEnable && enhancerEnabled);
         mPreset.setEnabled(!hifiEnable && enhancerEnabled);
+        mHifi.setEnabled(enhancerEnabled);
     }
 
     @Override
@@ -130,10 +131,13 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mSwitchBar.setActivated(isChecked);
 
         DiracUtils.setMusic(isChecked);
+        mHifi.setEnabled(isChecked);
+        mHeadsetType.setEnabled(isChecked);
+        mPreset.setEnabled(isChecked);
 
-        if (!DiracUtils.getHifiMode()) {
-            mHeadsetType.setEnabled(isChecked);
-            mPreset.setEnabled(isChecked);
+        if (!isChecked){
+            mHifi.setChecked(false);
+            DiracUtils.setHifiMode(0);
         }
     }
 
