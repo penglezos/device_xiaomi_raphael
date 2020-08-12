14d115748652f183ff05af722e63e8bf5670f7ac
LuK1337 <priv.luk@gmail.com>
2020-08-12 11:29:08 +0000

    raphael: init: Add 6, 8 & 12GB RAM support
    Change-Id: I466f1eccc964204dd51f1c044346ff2c707d6c37

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -13,6 +13,7 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
@@ -37,6 +38,28 @@ void property_override(char const prop[], char const value[], bool add = true) {
         __system_property_update(pi, value, strlen(value));
     else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void load_dalvik_properties() {
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+    if (sys.totalram > 7168ull * 1024 * 1024) {
+        // 8GB & 12GB RAM
+        property_override("dalvik.vm.heapstartsize", "32m");
+        property_override("dalvik.vm.heapgrowthlimit", "512m");
+        property_override("dalvik.vm.heapsize", "768m");
+        property_override("dalvik.vm.heapmaxfree", "64m");
+    } else {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heapgrowthlimit", "256m");
+        property_override("dalvik.vm.heapsize", "512m");
+        property_override("dalvik.vm.heapmaxfree", "32m");
+    }
+
+    property_override("dalvik.vm.heaptargetutilization", "0.5");
+    property_override("dalvik.vm.heapminfree", "8m");
 }
 
 void set_device_props(const std::string fingerprint, const std::string description,
@@ -90,4 +113,6 @@ void vendor_load_properties() {
             fp_desc,
             "Xiaomi", "raphael", "Mi 9T Pro");
     }
+
+    load_dalvik_properties();
 }
