4d77f42353bd7ab5a011509b5768ff0a88d4564d
Martin Liu <liumartin@google.com>
2020-09-09 10:40:31 +0800

    raphael: adjust zram write back policy
    The first default zram write back time is 3 hours which
    is for go device to quickly save more ram. For Pixel
    devices, we have more working set and could bring launch
    time impact if we write back too fast. Thus, adjust the
    first time write back time to 24 hours which is aligned
    with periodic write back time.
    
    Bug: 166739872
    Test: boot
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: If1398bc44db0619cf2e2be87d4813972c0454dba
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>
    
    Former-commit-id: 4fcd60a9fa547678f8b0b0d63ed0c7480864fad9

--- device.mk
+++ device.mk
@@ -380,7 +380,7 @@ PRODUCT_COPY_FILES += \
 # ZRAM writeback
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
-    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.first_wb_delay_mins=1440 \
     ro.zram.periodic_wb_delay_hours=24
 
 # QTI
