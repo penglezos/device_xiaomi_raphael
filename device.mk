f1157706191be03af6bae23e82a3104586290ee9
Murthy Nidadavolu <mnssr@codeaurora.org>
2020-09-16 16:46:05 +0530

    raphael: Update DRM HAL version
    Update Widevine DRM HAL to 1.3.
    
    Change-Id: Iebcf5c77fb68636352da9aa1332e0f5b41ae958f

--- device.mk
+++ device.mk
@@ -182,7 +182,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
