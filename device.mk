138a440ce0fdbc8b79c7f3a3071c8f484d7343bc
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    raphael: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- device.mk
+++ device.mk
@@ -17,4 +17,8 @@ PRODUCT_SOONG_NAMESPACES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor
+    android.hidl.base@1.0.vendor \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
