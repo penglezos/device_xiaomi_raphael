e9d41f21c1b278a8b14edfadec70ec741634c4a5
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    raphael: Drop no longer existing BluetoothQti
    Change-Id: I6bf3e1dda6fe5dc66f6fafdb32a1daecb9616c84
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -104,7 +104,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
-    BluetoothQti \
     libbthost_if \
     libldacBT_dec \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
