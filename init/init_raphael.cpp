f9dde89df299d7fe19f4a27ae8f3520ebd8bc0ac
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-09-21 04:25:10 +0000

    raphael: init: use walleye build fingerprint to pass cts
    also update to latest global vendor
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -89,8 +89,8 @@ void vendor_load_properties()
     }
 
     // fingerprint
-    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V11.0.8.0 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys");
+    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V12.0.1.0.QFKMIXM release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
 
     load_dalvikvm_properties();
 }
