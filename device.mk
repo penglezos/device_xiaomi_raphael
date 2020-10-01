6ca7ba20721bd2c28b590f0fd8fdde27e121b8e1
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    raphael: Remove packages at build time

--- device.mk
+++ device.mk
@@ -396,6 +396,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_raphael
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
