2dce8b47b99a522acfe9f4216bb52b0f015c88ab
Arian <arian.kulmer@web.de>
2020-10-11 22:26:25 +0200

    raphael: parts: Add a warning dialog for manual calibrations
    Change-Id: Id764e1cbe1a8c535c0039938097465d9a0dcd02b

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsFragment.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsFragment.java
@@ -16,8 +16,16 @@
 
 package org.lineageos.settings.popupcamera;
 
+import android.app.AlertDialog;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.CheckBox;
 
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
@@ -59,9 +67,48 @@ public class PopupCameraSettingsFragment extends PreferenceFragment
     @Override
     public boolean onPreferenceClick(Preference preference) {
         if (MOTOR_CALIBRATION_KEY.equals(preference.getKey())) {
-            mPopupCameraService.calibrateMotor();
+            SharedPreferences prefs = getActivity().getSharedPreferences(
+                    MOTOR_CALIBRATION_KEY, Activity.MODE_PRIVATE);
+            if (!prefs.getBoolean("popup_calibration_warning_hidden", false)) {
+                MotorCalibrationWarningDialog fragment = new MotorCalibrationWarningDialog();
+                fragment.show(getFragmentManager(), "motor_calibration_warning_dialog");
+            } else {
+                mPopupCameraService.calibrateMotor();
+            }
             return true;
         }
         return false;
+    }
+
+    private class MotorCalibrationWarningDialog extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            View view = getActivity().getLayoutInflater().inflate(
+                    R.layout.popup_calibration_warning, null);
+            CheckBox hideDialog = (CheckBox) view.findViewById(R.id.popup_calibration_warning_hide);
+
+            hideDialog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                    getActivity()
+                            .getSharedPreferences(MOTOR_CALIBRATION_KEY, Activity.MODE_PRIVATE)
+                            .edit()
+                            .putBoolean("popup_calibration_warning_hidden", isChecked)
+                            .commit();
+                }
+            });
+
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.popup_calibration_warning_title)
+                    .setMessage(R.string.popup_calibration_warning_text)
+                    .setView(view)
+                    .setPositiveButton(R.string.popup_camera_calibrate_now,
+                            (dialog, which) -> {
+                                mPopupCameraService.calibrateMotor();
+                                dialog.cancel();
+                            })
+                    .setNegativeButton(android.R.string.cancel, (dialog, which) -> dialog.cancel())
+                    .create();
+        }
     }
 }
