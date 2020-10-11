3d5f85c3c3af73c487b31241837574900b36879f
Arian <arian.kulmer@web.de>
2020-10-11 21:55:53 +0200

    raphael: parts: Add a setting to calibrate the motor
    Change-Id: I9f76124759bf8497b5cd72293b5c36f85b4d1787

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -167,7 +167,6 @@ public class PopupCameraService extends Service implements Handler.Callback {
             synchronized (mLock) {
                 if (status == Constants.MOTOR_STATUS_CALIB_OK
                         || status == Constants.MOTOR_STATUS_CALIB_ERROR) {
-                    mMotorCalibrating = false;
                     showCalibrationResult(status);
                 } else if (status == Constants.MOTOR_STATUS_PRESSED) {
                     updateMotor(Constants.CLOSE_CAMERA_STATE);
@@ -180,16 +179,26 @@ public class PopupCameraService extends Service implements Handler.Callback {
         }
     }
 
-    private void calibrateMotor() {
+    protected void calibrateMotor() {
         synchronized (mLock) {
-            if (mMotorCalibrating || mMotor == null)
+            if (mMotorCalibrating)
                 return;
+            if (mMotor == null) {
+                try {
+                    mMotor = IMotor.getService();
+                } catch (RemoteException e) {
+                    // Do nothing
+                }
+                if (mMotor == null)
+                    return;
+            }
             try {
                 mMotorCalibrating = true;
                 mMotor.calibration();
             } catch (RemoteException e) {
                 // Do nothing
             }
+            mHandler.postDelayed(() -> mMotorCalibrating = false, 7000);
         }
     }
 
