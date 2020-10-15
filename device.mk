01eaa3abe178f3d20e6904c452383da6f72d93a5
TheScarastic <warabhishek@gmail.com>
2020-10-15 13:18:13 +0000

    raphael: overlay: Adjust biometric dialog for 11
    Change-Id: Id8ee895e7fe8bb643f73eaa45116ecd64d697c38

--- device.mk
+++ device.mk
@@ -42,7 +42,8 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-system
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
