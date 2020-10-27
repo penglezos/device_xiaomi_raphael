f04ffbf640f94d96019fadadb19028d4cf4a4e37
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-10-27 04:05:52 +0000

    raphael: Switch to QTI vibrator Implementation
    Change-Id: I76c735b263f32bdfaf222bb2def11c44f9ebef36

--- device.mk
+++ device.mk
@@ -473,6 +473,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+      vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+      vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
