6fda7c4d063444210d01c07ccde2920e634372ad
Adhitya Mohan <me@adhityamohan.in>
2020-11-03 20:28:48 +0530

    raphael: releasetools: Ship vbmeta as well
    * No idea why this was never done
     fixes https://github.com/markakash/device_xiaomi_raphael/issues/1

--- releasetools.py
+++ releasetools.py
@@ -36,4 +36,5 @@ def AddImage(info, basename, dest):
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   return
