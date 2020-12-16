40408749a5de6c3a179ff45909dc2cfefafcca2d
Pig <pig.priv@gmail.com>
2020-12-16 10:28:31 +0800

    raphael: Move parts to /system_ext
    Change-Id: I564276782de81b78866a7d5c3cb3de3230231c85

--- parts/Android.mk
+++ parts/Android.mk
@@ -15,6 +15,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := XiaomiParts
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_USE_AAPT2 := true
