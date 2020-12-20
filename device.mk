b8ec316fa40e6cea4127b42d362ae4735edc15a7
TH779 <tongsj2333@gmail.com>
2020-12-20 11:00:04 +0800

    raphael: Use Google Camera Go for GApps version
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: I2872f648d67aae52266a7162d926b28488b7c467

--- device.mk
+++ device.mk
@@ -151,9 +151,16 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.xiaomi.hardware.motor@1.0.vendor
+
+ifeq ($(ARROW_GAPPS), true)
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt
+else
+PRODUCT_PACKAGES += \
+    Snap
+endif
 
 # Component overrides
 PRODUCT_COPY_FILES += \
