35223ef052a3b08c46e4470877c0472b3b4488d6
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-01-13 10:04:34 +0000

    raphael: rootdir: Checkout to LA.UM.9.1.r1-08600-SMxxx0.0
    Change-Id: I375abbc712f22a8e7b2aa49ab2318b25fdb30677

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -28,7 +28,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qcom.post_boot.sh
+LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
@@ -36,7 +36,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qcom.sh
+LOCAL_SRC_FILES    := etc/init.qcom.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
