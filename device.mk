8b13f57397eafb8fa38d545d84be932eb3d1b2a1
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-01-13 09:34:50 +0000

    raphael: Split and update properties from MIUI 21.2.3
    * Split them as per their specific partitions
    * Cleanup obsolete properties
    
    Change-Id: I83b1b198686aff4bbccca4665cc56ac3c7f0eaab

--- device.mk
+++ device.mk
@@ -192,14 +192,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
-# Enable incremental FS feature
-PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
-
-# Enable blurs
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.raphael
