4c7286a3a54aa5fdde57cc5474cce4fb7d7dc4c6
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-01-16 12:18:36 +0000

    [FIXME]raphael: nfc: Switch to AOSP Nfc
    * eSE is dead and nxp is pain
    
    Change-Id: Id914b696ac877e80bf2b3d7edd9421a7469f9eac

--- device.mk
+++ device.mk
@@ -352,17 +352,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # NFC
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/nxp/opensource/sn100x
-
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
-    com.gsma.services.nfc \
-    com.nxp.nfc.nq \
-    nfc_nci.nqx.default.hw \
     NfcNci \
-    Tag \
-    vendor.nxp.hardware.nfc@1.2-service
+    SecureElement \
+    Tag
+
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
