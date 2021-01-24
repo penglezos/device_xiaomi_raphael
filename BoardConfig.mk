12a5d6e6a12f835771a107d82795d012c1186118
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-24 14:02:20 +0530

    raphael: Set arch variant to armv8-2a-dotprod
    raphael:/ $ cat /proc/cpuinfo | grep Features -m 1
    Features        : fp asimd evtstrm aes pmull sha1 sha2 crc32 atomics fphp asimdhp cpuid asimdrdm lrcpc dcpop asimddp
    
    From this, it's evident that our CPU (Snapdragon 855/Kryo 485) supports
    all the features of ARMv8.2-DotProd [1]. Moreover AOSP sets this arch
    variant in their generic cortex-a55 ART target as well [2]. This should
    unlock some optimizations in certain code paths.
    
    [1]: https://en.wikichip.org/wiki/arm/armv8#ARMv8_Extensions_and_Processor_Features
    [2]: https://android.googlesource.com/device/generic/art/+/refs/heads/master/armv8_cortex_a55/BoardConfig.mk#23
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I1be144761ee94151e5b630f33ca6317fedb7b575
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: David Setiawan <fryevia@foxmail.com>
    
    Former-commit-id: 900d4c6273b5d5ef1d724a9070add0c752507e70

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ DEVICE_PATH := device/xiaomi/raphael
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
