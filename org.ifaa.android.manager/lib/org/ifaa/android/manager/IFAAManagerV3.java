1d94962a7355299667a067554e3316adb306787e
dianlujitao <dianlujitao@lineageos.org>
2021-02-15 07:24:52 +0100

    raphael: org.ifaa.android.manager: Access mlipay hal via a binder service
     * Untrusted apps are no longer allowed to directly access hwbinder on
       Pie, implement a system service as a middleware between mlipay hal
       and untrusted apps.
     * Xiaomi uses similar solution for MIUI on Pie.
    
    Change-Id: Ie52376b1f7bcd84d219fe73bceb4bdd6cc2b9980

--- org.ifaa.android.manager/lib/org/ifaa/android/manager/IFAAManagerV3.java
+++ org.ifaa.android.manager/lib/org/ifaa/android/manager/IFAAManagerV3.java
@@ -7,13 +7,10 @@ public abstract class IFAAManagerV3 extends IFAAManagerV2 {
     public static final String KEY_FINGERPRINT_FULLVIEW = "org.ifaa.ext.key.CUSTOM_VIEW";
     @UnsupportedAppUsage
     public static final String KEY_GET_SENSOR_LOCATION = "org.ifaa.ext.key.GET_SENSOR_LOCATION";
-    @UnsupportedAppUsage
-    public static final String VALUE_FINGERPRINT_DISABLE = "disable";
-    @UnsupportedAppUsage
-    public static final String VLAUE_FINGERPRINT_ENABLE = "enable";
+    @UnsupportedAppUsage public static final String VALUE_FINGERPRINT_DISABLE = "disable";
+    @UnsupportedAppUsage public static final String VLAUE_FINGERPRINT_ENABLE = "enable";
 
-    @UnsupportedAppUsage
-    public abstract String getExtInfo(int authType, String keyExtInfo);
+    @UnsupportedAppUsage public abstract String getExtInfo(int authType, String keyExtInfo);
 
     @UnsupportedAppUsage
     public abstract void setExtInfo(int authType, String keyExtInfo, String valExtInfo);
--- org.ifaa.android.manager/src/org/ifaa/android/manager/IFAAManagerV4.java
+++ org.ifaa.android.manager/src/org/ifaa/android/manager/IFAAManagerV4.java
@@ -3,9 +3,7 @@ package org.ifaa.android.manager;
 import android.compat.annotation.UnsupportedAppUsage;
 
 public abstract class IFAAManagerV4 extends IFAAManagerV3 {
-    @UnsupportedAppUsage
-    public abstract int getEnabled(int i);
+    @UnsupportedAppUsage public abstract int getEnabled(int i);
 
-    @UnsupportedAppUsage
-    public abstract int[] getIDList(int i);
+    @UnsupportedAppUsage public abstract int[] getIDList(int i);
 }
