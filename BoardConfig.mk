c9b2980038f1ee60eff65cb14ca88736e1ec0943
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-02-16 16:19:43 +0100

    raphael: Switch to enforcing SELinux
    Change-Id: Ifbe552206d2cd0d42e5505557468ed836976cd73

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,7 +115,7 @@ BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
