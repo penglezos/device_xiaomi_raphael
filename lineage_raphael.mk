fbc9b0725ecc94feb8510e30dc7fe74185d76cc8
Jorge Lucas <jorgelucas@pixelexperience.org>
2021-04-14 00:50:27 -0300

    raphael: Add missing TARGET_BOOT_ANIMATION_RES
    Change-Id: I3879ff51a5484097ca073997599e754363eaa6ef

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -17,6 +17,7 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_raphael
