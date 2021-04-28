02a85491ce72fd0bda940429fe905f2924f3d5af
OdSazib <odsazib@gmail.com>
2021-04-29 00:48:05 +0530

    raphael: Build mock power stats service
    Fixes
    * E BatteryStatsService: Unable to load Power Hal or power.stats HAL
    * I/hwservicemanager(556): getTransport: Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest
    
    This reverts commit e7a59c18bdd91f3f555bc6cd180a988986b85573.

--- device.mk
+++ device.mk
@@ -349,7 +349,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/msmnile/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
