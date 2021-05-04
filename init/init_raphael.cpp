9cc65627084046facf6b08416810c3a4ba0fae90
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-05-04 06:55:02 +0200

    raphael: Dont set build fingerprint
    Change-Id: I84bf61950e0d8c7c4d5f4819cd709963679c6fa3

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -105,21 +105,15 @@ void vendor_load_properties() {
     if (region == "GLOBAL") {
         model = "Mi 9T Pro";
         device = "raphael";
-        fingerprint =
-                "Xiaomi/raphael/raphael:10/QKQ1.190825.002/V12.0.4.0.QFKMIXM:user/release-keys";
         description = "raphael-user 10 QKQ1.190825.002 V12.0.4.0.QFKMIXM release-keys";
         mod_device = "raphael_global";
     } else if (region == "CN") {
         model = "Redmi K20 Pro";
         device = "raphael";
-        fingerprint =
-                "Xiaomi/raphael/raphael:10/QKQ1.190825.002/V12.0.6.0.QFKCNXM:user/release-keys";
         description = "raphael-user 10 QKQ1.190825.002 V12.0.6.0.QFKCNXM release-keys";
     } else if (region == "INDIA") {
         model = "Redmi K20 Pro";
         device = "raphaelin";
-        fingerprint =
-                "Xiaomi/raphaelin/raphaelin:10/QKQ1.190825.002/V12.0.4.0.QFKINXM:user/release-keys";
         description = "raphaelin-user 10 QKQ1.190825.002 V12.0.4.0.QFKINXM release-keys";
         mod_device = "raphaelin_in_global";
     }
@@ -128,10 +122,7 @@ void vendor_load_properties() {
     
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    fingerprint = "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys";
-    description = "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys";
 
-    set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
     set_ro_product_prop("model", model);
     property_override("ro.build.description", description.c_str());
