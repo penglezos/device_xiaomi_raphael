1e43e2df9a25d432348803b7cf0972f6bc07d177
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-09 16:04:34 -0400

    raphael: Simplify AVB flag logic
    * AVB's `--flags 3` argument builds a disabled VBMeta with
      disabled hashtree, so no need to set both flags individually.
    
    Change-Id: Ibe9ee322a5af58f13e76d53e18dee49451fe8838

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,8 +144,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
