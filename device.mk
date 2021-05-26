0129efea383352d4e8e2802ca7352b702d4a9a88
Adhitya Mohan <me@adhityamohan.in>
2021-05-26 12:47:12 +0530

    raphael: Remove headphone keylayout file
    fixed in kernel: https://github.com/yaap/kernel_xiaomi_sm8150/commit/c518c411e8c556e31fad52af272eb43949a68dbd

--- device.mk
+++ device.mk
@@ -268,8 +268,7 @@ PRODUCT_COPY_FILES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/sm8150-tavil-snd-card_Button_Jack.kl:system/usr/keylayout/sm8150-tavil-snd-card_Button_Jack.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
 # Lights
 PRODUCT_PACKAGES += \
