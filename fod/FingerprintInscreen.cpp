a3d92ff92784e85407ac186c357332422ee26e82
Adhitya Mohan <me@adhityamohan.in>
2021-05-27 20:00:19 +0530

    raphael: Don't set dim layer on fod view
    * causes random flickering when screen is turned on / night light

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -152,7 +152,6 @@ Return<void> FingerprintInscreen::onRelease() {
 
 Return<void> FingerprintInscreen::onShowFODView() {
     set(FOD_STATUS_PATH, FOD_STATUS_ON);
-    WriteToFile(DIM_LAYER_HBM_PATH, 1);
     return Void();
 }
 
