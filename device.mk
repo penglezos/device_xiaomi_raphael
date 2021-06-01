99819d77d0bffed4d5939691e82bd26fd3b7d740
TH779 <i@779.moe>
2021-06-02 01:51:49 +0800

    raphael: Import QTI privapp permissions XML
     * From LA.UM.9.1.r1-09600-SMxxx0.0.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Icd54970a5cdc04cd5ccf7639c94bd83f41f961e3

--- device.mk
+++ device.mk
@@ -99,6 +99,7 @@ PRODUCT_COPY_FILES += \
 
 # QTI
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # WiFi
