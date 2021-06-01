c9bc3d1aaeb13bc75bf191c5f7f774b3b10bcc22
TH779 <i@779.moe>
2021-06-02 01:39:48 +0800

    raphael: Import WiFi configs
     * From V12.5.4.0.RFKCNXM.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I9aeb587f26d5732165b8288482c78c1e1e19f25c

--- device.mk
+++ device.mk
@@ -77,3 +77,9 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder \
     libhwbinder.vendor
+
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
