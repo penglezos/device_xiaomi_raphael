5162ee98284776a18d7681e6312f44372a702af5
TH779 <i@779.moe>
2021-06-07 08:52:17 +0800

    raphael: Build vendor.xiaomi.hardware.motor@1.0 from source
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I84efc8b8e4ff856f666186215e90cc2556e850e6

--- device.mk
+++ device.mk
@@ -151,7 +151,8 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     Snap \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.xiaomi.hardware.motor@1.0.vendor
 
 # Component overrides
 PRODUCT_COPY_FILES += \
