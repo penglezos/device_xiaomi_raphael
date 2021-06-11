4f4e52ebc648dd7c8f6fb9d759a5bbe3a5f42a9e
Adhitya Mohan <me@adhityamohan.in>
2021-06-11 10:52:39 +0530

    raphael: Add qcdm calib file
    * with some extra profiles on top
    
    
    Former-commit-id: 51ac5779f998b1d4555c54d11f73ad1917ae214a

--- device.mk
+++ device.mk
@@ -179,7 +179,8 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    $(LOCAL_PATH)/configs/qdcm_calib_data_samsung_ea8076_fhd_cmd_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_samsung_ea8076_fhd_cmd_dsi_panel.xml
 
 # DRM
 PRODUCT_PACKAGES += \
