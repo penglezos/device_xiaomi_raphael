d75ba934a13ab50819aef3a5443a230526eb4700
TH779 <i@779.moe>
2021-07-23 22:06:19 +0800

    raphael: Initial device specific sepolicies
     * Based on Xiaomi sm6150-common.
    
    Co-authored-by: Isaac Chen <tingyi364@gmail.com>
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Pig <pig.priv@gmail.com>
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Ie7560826379925e159693eddd7b4ac1dcd9ebc93

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,10 @@ VENDOR_SECURITY_PATCH := 2021-04-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
