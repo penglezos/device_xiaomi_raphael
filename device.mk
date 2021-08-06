ffcea50e85b2e4b3c2cc10d9bfa09ed9013d184f
PIPIPIG233666 <pig.priv@gmail.com>
2021-08-06 07:40:29 +0000

    raphael:Build thermal v2.0
     * android.hardware.thermal@1.0::IThermal/default is deprecated in
     compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: I140be2a9307e8cbf475c35fb561dd675f6458c4d
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    
    Former-commit-id: a34d94fae861bf0d8068113fbac02db5cc358537

--- device.mk
+++ device.mk
@@ -391,6 +391,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/msmnile/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.xiaomi_msmnile
+
 # ZRAM writeback
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
