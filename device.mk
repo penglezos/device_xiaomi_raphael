62b0978900567a4973ae2da452ab8eb618bd45f8
TH779 <i@779.moe>
2021-08-06 19:05:15 +0800

    raphael: Import HotwordEnrollement permissions XML
     * Extracted from the privapp-permissions-google-product.xml file of
       V12.5.1.0.RFKMIXM.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I872707a9633c087b119bac2f5bb07891969d7ae3

--- device.mk
+++ device.mk
@@ -220,6 +220,10 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# HotwordEnrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
