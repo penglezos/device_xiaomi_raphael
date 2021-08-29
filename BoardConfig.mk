eaf7592fee30f0d2cd39a2c909f521b522008d5b
penglezos <panagiotisegl@gmail.com>
2021-08-29 21:14:47 +0300

    raphael: Remove TARGET_USES_INTERACTION_BOOST
    Change-Id: Ibfd37f7785ef39fd576751d95bfd0537c81d95d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,7 +124,6 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-TARGET_USES_INTERACTION_BOOST := true
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
