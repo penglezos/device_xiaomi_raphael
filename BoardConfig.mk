f5a3ff002b20e01071d7738fb9d80918d9ef5b70
TH779 <i@779.moe>
2021-08-29 20:08:30 +0800

    raphael: Switch to kernel node based DT2W impl
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I9c0ba44a0d5dff112e7fafc21c9dd8623fa6494d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,7 +123,7 @@ TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 TARGET_USES_INTERACTION_BOOST := true
 
 # QCOM
