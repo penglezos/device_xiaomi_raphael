b3dd0bc3fcc6efb46f58c927ff658a956593250e
kubersharma001 <kubersharma001@gmail.com>
2021-09-08 04:28:41 +0000

    raphael: Update blobs from V12.5.2.0.RFKMIXM
    Change-Id: I2b90ee88801e4638caab4b1b5f80dce878384d0f

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -26,4 +26,4 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.5.0.RFKCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.2.0.RFKMIXM:user/release-keys
