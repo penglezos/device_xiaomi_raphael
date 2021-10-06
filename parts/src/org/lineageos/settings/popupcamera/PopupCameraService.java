e2b9e97e767c14a2c47f8de27a15560008d67d28
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:23:48 +0000

    raphael: Change method used for popup
    android s reports onCameraUnavailable during shutdown which causes
    camera to popup during shutdown or reboot hence switch to proper
    methods
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    
    Former-commit-id: 18589382e07a6800d3896c4643970218dcc46144

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -95,8 +95,8 @@ public class PopupCameraService extends Service implements Handler.Callback {
     private CameraManager.AvailabilityCallback availabilityCallback =
             new CameraManager.AvailabilityCallback() {
                 @Override
-                public void onCameraAvailable(@NonNull String cameraId) {
-                    super.onCameraAvailable(cameraId);
+                public void onCameraClosed(@NonNull String cameraId) {
+                    super.onCameraClosed(cameraId);
                     if (cameraId.equals(Constants.FRONT_CAMERA_ID)) {
                         mClosedEvent = SystemClock.elapsedRealtime();
                         if (SystemClock.elapsedRealtime() - mOpenEvent
@@ -110,8 +110,8 @@ public class PopupCameraService extends Service implements Handler.Callback {
                 }
 
                 @Override
-                public void onCameraUnavailable(@NonNull String cameraId) {
-                    super.onCameraAvailable(cameraId);
+                public void onCameraOpened(@NonNull String cameraId, @NonNull String packageId) {
+                    super.onCameraOpened(cameraId, packageId);
                     if (cameraId.equals(Constants.FRONT_CAMERA_ID)) {
                         mOpenEvent = SystemClock.elapsedRealtime();
                         if (SystemClock.elapsedRealtime() - mClosedEvent
