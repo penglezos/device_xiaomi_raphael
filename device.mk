9ca811f2871df11d2724f35fdd0daaa8794d5513
TH779 <i@779.moe>
2021-10-17 07:29:00 +0000

    raphael: Nuke Xiaomi USB changes
      This is a combination of 3 commits.
    
      This is the 1st commit message:
    
        Revert "raphael: keep persist.vendor.usb.config in sync with vendor.usb.mimode"
    
        This reverts commit 6905abc39d238c31845702a81c9458a581fc37a3.
    
        Signed-off-by: TH779 <i@779.moe>
        Change-Id: I4ee01c0e6af9438ce90293d92f95e67545fda132
    
      This is the commit message #2:
    
        Revert "raphael: Clear persist.vendor.usb.config on boot"
    
        This reverts commit 87a8c95833b715fa86937bb644c07411f00e8c8b.
    
        Signed-off-by: TH779 <i@779.moe>
        Change-Id: I495b734629bbc451cf839c171e9e36f85d42ce30
    
      This is the commit message #3:
    
        Revert "raphael: Import Xiaomi changes over USB scripts"
    
        This reverts commit 9b54cae56cf9a41371928c8c9edccf47172d5a55.
    
        Signed-off-by: TH779 <i@779.moe>
        Change-Id: Ibe2bd71d8f5a278129d52653f08f4ac57243833a
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I22216c07ddbb740f1e08b58d871e99f6805bafd5

--- device.mk
+++ device.mk
@@ -237,7 +237,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.class_main.sh \
-    init.mi.usb.sh \
     init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -18,14 +18,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mi.usb.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mi.usb.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.class_core.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
