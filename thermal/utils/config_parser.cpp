aa1bec9b6cfffd96632b71291ea32fbe278b8169
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-10-20 18:30:00 +0000

    thermal: Use Json::CharReader instead of deprecated Json::Reader
    Former-commit-id: 1e60583b907d9f5fd5273740067a292be020895b

--- thermal/utils/config_parser.cpp
+++ thermal/utils/config_parser.cpp
@@ -68,9 +68,11 @@ std::map<std::string, SensorInfo> ParseSensorInfo(std::string_view config_path) 
     }
 
     Json::Value root;
-    Json::Reader reader;
+    Json::CharReaderBuilder builder;
+    std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+    std::string errorMessage;
 
-    if (!reader.parse(json_doc, root)) {
+    if (!reader->parse(&*json_doc.begin(), &*json_doc.end(), &root, &errorMessage)) {
         LOG(ERROR) << "Failed to parse JSON config";
         return sensors_parsed;
     }
@@ -243,9 +245,11 @@ std::map<std::string, CoolingType> ParseCoolingDevice(std::string_view config_pa
     }
 
     Json::Value root;
-    Json::Reader reader;
+    Json::CharReaderBuilder builder;
+    std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+    std::string errorMessage;
 
-    if (!reader.parse(json_doc, root)) {
+    if (!reader->parse(&*json_doc.begin(), &*json_doc.end(), &root, &errorMessage)) {
         LOG(ERROR) << "Failed to parse JSON config";
         return cooling_devices_parsed;
     }
