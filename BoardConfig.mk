13ff495e34b59440bfa402319a5eccfb5938380e
isnrdra <isanoorendra@gmail.com>
2021-11-07 13:11:07 +0530

    raphael: Enable AOSP surfaceflinger
    * AOSP SurfaceFlinger is smoother than CAF
    
    Signed-off-by: Dyneteve <sound0020@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    Signed-off-by: isnrdra <isanoorendra@gmail.com>
    Signed-off-by: frost <optimus7907@gmail.com>
    
    Former-commit-id: 2d5d9ab7ecd033622f99d835ac29565a2cc5505a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -222,6 +222,9 @@ USE_SENSOR_MULTI_HAL := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/xiaomi/raphael-sepolicy/raphael-sepolicy.mk
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Vendor init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_raphael
 TARGET_RECOVERY_DEVICE_MODULES := libinit_raphael
