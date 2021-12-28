0ba7f4add8a9d2ec11eeee13535ba72d3f85ec2b
penglezos <panagiotisegl@gmail.com>
2021-12-28 12:16:05 +0000

    raphael: rootdir: Add restricted cpuset

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -138,6 +138,7 @@ case "$target" in
 	echo 4-7 > /dev/cpuset/foreground/boost/cpus
 	echo 0-2,4-7 > /dev/cpuset/foreground/cpus
 	echo 0-7 > /dev/cpuset/top-app/cpus
+        echo 0-3 > /dev/cpuset/restricted/cpus
 
 	# Turn off scheduler boost at the end
 	echo 0 > /proc/sys/kernel/sched_boost
