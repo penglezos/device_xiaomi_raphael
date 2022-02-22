138f7dcba8c91217a9126e0be328e559ec61c93a
penglezos <panagiotisegl@gmail.com>
2022-02-22 19:20:19 +0200

    raphael: Drop biometric fingerprint view * Not needed

--- device.mk
+++ device.mk
@@ -39,8 +39,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay-arrow
 
 # ANT+
 PRODUCT_PACKAGES += \
