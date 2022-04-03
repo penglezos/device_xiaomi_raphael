d11f11d73f50cf5101c674c2fa08f835efa7d456
penglezos <panagiotisegl@gmail.com>
2022-04-03 23:03:04 +0300

    raphael: Switch to full_base_telephony

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Setup dalvik vm configs
