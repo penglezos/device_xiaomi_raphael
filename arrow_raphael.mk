0acc2237768bb6dcd159351ab9728c3b65310a8c
penglezos <panagiotisegl@gmail.com>
2022-04-03 23:08:37 +0300

    raphael: Move device inherit-products to arrow_raphael

--- arrow_raphael.mk
+++ arrow_raphael.mk
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
