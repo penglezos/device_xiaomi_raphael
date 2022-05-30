bef4308d3729c5d0e990318bf51ed06f9cbdb603
penglezos <panagiotisegl@gmail.com>
2022-05-30 18:57:59 +0300

    raphael: Switch to source built perf

--- device.mk
+++ device.mk
@@ -364,8 +364,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2 \
-    android.hardware.power@1.2.vendor \
+    vendor.qti.hardware.perf@2.2.vendor \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
