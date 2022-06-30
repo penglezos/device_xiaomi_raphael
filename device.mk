0ef2fd629682cc04cd4883d969d517da56f8657b
SebaUbuntu <barezzisebastiano@gmail.com>
2022-06-30 06:22:33 +0000

    raphael: Drop atrace HIDL
    - This hal is essentially unnecessary as we don't need much tracing on a production build.

--- device.mk
+++ device.mk
@@ -40,10 +40,6 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     com.dsi.ant@1.0.vendor
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
