d658656fbb13eb868247f6e2780814b81e5da6ee
penglezos <panagiotisegl@gmail.com>
2022-09-20 18:55:08 +0300

    raphael: Opt-out from LLVM binutils
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/raphael
 TARGET_KERNEL_CONFIG := raphael_defconfig
 TARGET_KERNEL_CLANG_VERSION := clang-r416183b1
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r416183b1
+TARGET_KERNEL_LLVM_BINUTILS := false
 
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
