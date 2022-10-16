370852e8d6b272166ef95b75a360576e6187e3e4
penglezos <panagiotisegl@gmail.com>
2022-10-16 18:26:32 +0300

    raphael: Override build fingerprint and description * To pass SafetyNet
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -37,5 +37,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="raphael"
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+    BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
