a73faf72f5f04c76b701da4ef23724579490badf
penglezos <panagiotisegl@gmail.com>
2022-10-19 22:49:50 +0300

    raphael: Build Trust HAL
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- device.mk
+++ device.mk
@@ -464,6 +464,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
