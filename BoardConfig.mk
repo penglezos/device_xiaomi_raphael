b1de06ac02164309f951eb4ef941dac57eb36d8d
penglezos <panagiotisegl@gmail.com>
2022-11-07 19:08:11 +0200

    raphael: Switch to clang-r416183b as default compiler
    * As of commit 60d47 LineageOS syncs that version. Until I make clang-14 work
    
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,8 +131,8 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/raphael
 TARGET_KERNEL_CONFIG := raphael_defconfig
-TARGET_KERNEL_CLANG_VERSION := clang-r416183b1
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r416183b1
+TARGET_KERNEL_CLANG_VERSION := r416183b
+TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/kernel/$(HOST_PREBUILT_TAG)/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_LLVM_BINUTILS := false
 
 # Keystore
