ac215b2649434c87a94e57cfaed65304d2700cf0
penglezos <panagiotisegl@gmail.com>
2023-04-15 14:13:27 +0300

    raphael: parts: Remove doze - Switched to proper sensor based implementation
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -21,7 +21,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -32,7 +31,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
-        DozeUtils.checkDozeService(context);
         PopupCameraUtils.startService(context);
     }
 }
