3bfba0c39204aad29a0b9224e487ea51399c9870
penglezos <panagiotisegl@gmail.com>
2023-04-15 14:09:44 +0300

    raphael: Build Xiaomi sensors implementation
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- device.mk
+++ device.mk
@@ -386,7 +386,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service \
     android.hardware.sensors@2.0 \
     android.hardware.sensors@2.1 \
