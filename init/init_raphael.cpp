c69f7722408605b9d5c9f41e26540ff51ebe8c48
penglezos <panagiotisegl@gmail.com>
2023-07-22 17:43:15 +0300

    init_raphael: Define ro.boot.hardware.sku and ro.boot.product.hardware.sku
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -122,6 +122,8 @@ void vendor_load_properties() {
 
     set_ro_product_prop("device", device);
     set_ro_product_prop("model", model);
+    property_override("ro.boot.hardware.sku", device.c_str());
+    property_override("ro.boot.product.hardware.sku", device.c_str());
     property_override("ro.build.description", description.c_str());
     if (mod_device != "") {
         property_override("ro.product.mod_device", mod_device.c_str());
