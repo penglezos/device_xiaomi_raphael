dcf450a4e429042c5a743dec601ad61228e620a8
penglezos <panagiotisegl@gmail.com>
2023-08-11 20:00:02 +0300

    Revert "raphael: parts: Implement DC dimming"
    This reverts commits:
    c231dfa
    e24d018
    533ac61
    
    Will use LiveDisplay's Antiflicker & HBM mode which will be implemented on the following commits. Keep this for future reference.
    
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,28 +20,17 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.SystemProperties;
 import android.util.Log;
-import androidx.preference.PreferenceManager;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
-import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
-    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
-    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/msm_fb_ea_enable";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         PopupCameraUtils.startService(context);
-
-        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
-        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
     }
 }
